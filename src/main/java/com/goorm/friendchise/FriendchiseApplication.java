9077c810cdaf742e97e9acb59fd34ef25e15248b
fbehddn <tom0611@naver.com>
2025-02-04 18:04:03 +0900

    chore: initialize project setup

---
+++ src/main/java/com/goorm/friendchise/FriendchiseApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.friendchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendchiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendchiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goorm/friendchise/FriendchiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.friendchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendchiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
