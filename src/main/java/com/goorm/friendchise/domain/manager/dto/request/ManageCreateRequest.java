821132ab34023d0f9e98fbc3ff72abfd0eed0235
유동우 <108855080+fbehddn@users.noreply.github.com>
2025-02-13 13:16:09 +0900

    fix: Role 필드 검증 오류 수정 (`@NotBlank` → `@NotNull`) (#59)

--- src/main/java/com/goorm/friendchise/domain/manager/dto/request/ManageCreateRequest.java
+++ src/main/java/com/goorm/friendchise/domain/manager/dto/request/ManageCreateRequest.java
@@ -3,6 +3,7 @@ package com.goorm.friendchise.domain.manager.dto.request;
 import com.goorm.friendchise.domain.manager.domain.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -19,7 +20,7 @@ public record ManageCreateRequest(
 	String password,
 
 	@Schema(description = "역할", example = "HEADQUARTER", requiredMode = REQUIRED)
-	@NotBlank
+	@NotNull
 	Role role,
 
 	@Schema(description = "본사ID", example = "1")
