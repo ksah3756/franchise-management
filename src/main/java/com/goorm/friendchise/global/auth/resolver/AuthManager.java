e8bf1538de265e74c3065d126a0ec985ec19601c
ksah3756 <ksah3756@gmail.com>
2025-03-04 17:32:31 +0900

    fix: swagger Manager parameter 무시하도록 수정

--- src/main/java/com/goorm/friendchise/global/auth/resolver/AuthManager.java
+++ src/main/java/com/goorm/friendchise/global/auth/resolver/AuthManager.java
@@ -1,9 +1,12 @@
 package com.goorm.friendchise.global.auth.resolver;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.*;
 
 @Target({ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Parameter(hidden = true)
 @Documented
 public @interface AuthManager {
     boolean errorOnInvalidType() default true;
